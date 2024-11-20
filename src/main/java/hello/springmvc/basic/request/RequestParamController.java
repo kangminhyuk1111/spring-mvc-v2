9daf91659c23fa6c2aa0c14f00bf1a61d619ed38
kangminhyuk1111 <rkdglqkr12@naver.com>
2024-11-20 16:53:49 +0900

    modelattribute

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,12 +1,10 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.Map;
@@ -63,5 +61,19 @@ public class RequestParamController {
     public String requestParamMap(@RequestParam Map<String, String> map) {
         log.info("username : {}, age : {}", map.get("username"), map.get("age"));
         return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute final HelloData helloData) {
+        log.info("username : {}, age : {}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public HelloData modelAttributeV2(final HelloData helloData) {
+        log.info("username : {}, age : {}", helloData.getUsername(), helloData.getAge());
+        return helloData;
     }
 }
